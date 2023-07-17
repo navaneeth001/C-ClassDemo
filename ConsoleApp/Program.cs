8e2fc1bdcd3f7a85ee78d298ee9d19e336aa82d5
Navaneeth Prakash <navaneethpqln@gmail.com>
2023-07-17 15:49:06 +0530

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using DemoLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo demo = new Demo();
+            demo.Message();
+        }
+    }
+}
---
+++ Demo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoLibrary
+{
+    public class Demo
+    {
+        public void Message()
+        {
+            Console.WriteLine("I am great");
+        }
+    }
+}
